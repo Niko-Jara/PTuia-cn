103c52124c14fcc98c7b0f143f4b8af679c7a283
Niko-Jara <jaramnikol@gmail.com>
2021-07-27 16:13:57 -0600

    Agregar archivos de proyecto.

---
+++ WebProjectFenix/WebProjectFenix/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebProjectFenix
+{
+    //RENDERIZA TODAS LAS HOJAS DE ESTILO QUE HAN SIDO DECLARADAS EN EL STYLE RENDER CONTENT CSS
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //..SCRIPS
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/js/scripts.js",
+                      "~/Scripts/js/efect.js"));      //Diseño de Plantilla por defecto
+
+
+
+            //..STYLES  revisar
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/Styles/styles.css"));  //Diseño de Plantilla por defecto
+        }
+    }
+}
---
+++ WebProjectFenix/WebProjectFenix/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebProjectFenix.MvcApplication" Language="C#" %>
